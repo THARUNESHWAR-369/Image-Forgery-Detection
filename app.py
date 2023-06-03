5d61345321ea942162a9615cb9e828c82ae0fdc5
THARUNESHWAR S <a.tharuneshwar.369@gmail.com>
2023-06-03 20:28:46 +0530

    bg added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session, redirect, url_for
 import cv2
 import hashlib
 import numpy as np
 import base64
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "qweoriuiooiqweuridsfhn897829347nc83"
 
 def calculate_md5(image_data):
     md5_hash = hashlib.md5(image_data).hexdigest()
@@ -27,26 +28,50 @@ def detect_forgery(original_image_data, modified_image_data):
 
 @app.route('/')
 def home():
-    return render_template('login.html')
-
+    if session.get("auth"):
+        return redirect(url_for("admin"))
+    return render_template('login.html', error = None)
 @app.route('/login', methods=['POST'])
 def login():
+    error= None
+    if session.get("auth"):
+        return redirect(url_for("admin"))
     # Handle login functionality here
     username = request.form['username']
     password = request.form['password']
-    return render_template('admin.html')
+    if username == 'admin' and password == "admin":
+        session['auth'] = True
+        return redirect(url_for("admin"))
+    else:
+        error = " *Invalid Password"
+    return render_template('login.html', error = error)
 
-@app.route('/admin', methods=['POST'])
+@app.route('/admin')
 def admin():
-    original_image = request.files['original_image']
-    modified_image = request.files['modified_image']
+    if not session.get("auth"):
+        return redirect(url_for("login"))
+    return render_template('admin.html', result=None, modified_image_encoded=None)
 
-    original_image_data = original_image.read()
-    modified_image_data = modified_image.read()
 
-    result, modified_image_encoded = detect_forgery(original_image_data, modified_image_data)
+@app.route("/detect", methods=["POST",])
+def detect():
+    if session.get("auth"):
+        original_image = request.files['original_image']
+        modified_image = request.files['modified_image']
 
-    return render_template('admin.html', result=result, modified_image_encoded=modified_image_encoded)
+        original_image_data = original_image.read()
+        modified_image_data = modified_image.read()
+
+        result, modified_image_encoded = detect_forgery(original_image_data, modified_image_data)
+
+        return render_template('admin.html', result=result, modified_image_encoded=modified_image_encoded)
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if session.get("auth"):
+        del session['auth']
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
